5a385a7832b665b7a2130d41b5f30a3ea5869ecc
sacurio <sandy.acurio@gmail.com>
2023-08-22 20:18:44 -0500

    Implement `RegisterUser` and `ValidateUser` methods

--- backend/internal/app/user/user_service.go
+++ backend/internal/app/user/user_service.go
@@ -1,5 +1,10 @@
 package user
 
+import (
+	"crypto/md5"
+	"fmt"
+)
+
 type (
 	// UserService
 	UserService interface {
@@ -23,10 +28,40 @@ func NewUserService(userRepo UserRepository, userValidator UserValidator) UserSe
 
 // RegisterUser persists a new User to data source.
 func (us *userService) RegisterUser(username, email, password string) error {
+	// Validar el usuario
+	newUser := &User{
+		Username: username,
+		Email:    email,
+		Pwd:      password,
+	}
+
+	if err := us.userValidator.Validate(newUser); err != nil {
+		return err
+	}
+
+	hashedPassword := md5.Sum([]byte(password))
+	newUser.Pwd = fmt.Sprintf("%x", hashedPassword)
+
+	if err := us.userRepo.Create(newUser); err != nil {
+		return err
+	}
+
 	return nil
 }
 
 // ValidateUser determines if an User exists on data source.
 func (us *userService) ValidateUser(username, password string) (bool, error) {
-	return false, nil
+	hashedPassword := md5.Sum([]byte(password))
+	strHashedPassword := fmt.Sprintf("%x", hashedPassword)
+
+	user, err := us.userRepo.FindByUsernameAndPassword(username, strHashedPassword)
+	if err != nil {
+		return false, err
+	}
+
+	if user == nil {
+		return false, nil // Usuario no encontrado
+	}
+
+	return true, nil
 }
