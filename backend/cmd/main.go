c8e5ad30a8ed67d2b0d657b532ac5bcb66c34e62
sacurio <sandy.acurio@gmail.com>
2023-08-23 04:32:39 -0500

    Use `RabbitMQ` infraestructure from main

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"github.com/sacurio/jb-challenge/internal/app/message"
 	"github.com/sacurio/jb-challenge/internal/app/user"
 	"github.com/sacurio/jb-challenge/internal/config"
-	"github.com/sacurio/jb-challenge/internal/db"
-	"github.com/sacurio/jb-challenge/internal/server"
+	"github.com/sacurio/jb-challenge/internal/infraestructure/db"
+	"github.com/sacurio/jb-challenge/internal/infraestructure/rabbitmq"
+	"github.com/sacurio/jb-challenge/internal/infraestructure/server"
 	"github.com/sacurio/jb-challenge/internal/util"
 	"github.com/sirupsen/logrus"
 )
@@ -21,10 +23,20 @@ func main() {
 	dbHandler := db.NewDatabase(app.DBConfig.User, app.DBConfig.Pwd, app.DBConfig.Port, app.DBConfig.Host, app.DBConfig.DbName, log)
 	dbHandler.SetupDB()
 
+	rabbitMQConfig := rabbitmq.NewRabbitMQConfig(app.RMQConfig.User, app.RMQConfig.Password, app.RMQConfig.Host, app.RMQConfig.Port)
+	rabbitMQHandler, err := rabbitmq.NewRabbitMQ(rabbitMQConfig, log)
+	if err != nil {
+		log.Panicf("Error on connecting to RabbitMQ service: %s", err.Error())
+	}
+
+	defer rabbitMQHandler.Close()
+
+	messageService := message.NewMessageService(rabbitMQHandler)
+
 	userRepo := user.NewUserRepository(dbHandler.DB)
 	userValidator := user.NewUserValidator()
 	userUserService := user.NewUserService(userRepo, userValidator)
-	srv := server.NewServer(util.ValidateStringNotEmpty(app.Port, util.DefaultPort), userUserService, log)
 
+	srv := server.NewServer(util.ValidateStringNotEmpty(app.Port, util.DefaultPort), userUserService, messageService, log)
 	srv.StartHTTPServer()
 }
