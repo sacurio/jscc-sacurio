87d1a56f6005ed4c49759108f58143d0168309c8
sacurio <sandy.acurio@gmail.com>
2023-08-22 20:18:11 -0500

    Improve a find user method

--- backend/internal/app/user/user_repository.go
+++ backend/internal/app/user/user_repository.go
@@ -6,7 +6,7 @@ type (
 	// UserRepository defines the methods to be implemented by the user service.
 	UserRepository interface {
 		Create(user *User) error
-		FindByUsername(username string) (*User, error)
+		FindByUsernameAndPassword(username, pwd string) (*User, error)
 	}
 
 	userRepository struct {
@@ -21,13 +21,27 @@ func NewUserRepository(db *gorm.DB) UserRepository {
 	}
 }
 
-// TODO: Probably be deleted
-// Create...
-func (r *userRepository) Create(user *User) error {
+// Create a new User through the use of Gorm.
+func (ur *userRepository) Create(user *User) error {
+	result := ur.db.Create(user)
+	if result.Error != nil {
+		return result.Error
+	}
+
 	return nil
 }
 
-// FindByUsername determines if an user exists on data source, based on its username value and returns a *User instance.
-func (r *userRepository) FindByUsername(username string) (*User, error) {
-	return nil, nil
+// FindByUsernameAndPassword determines if an user exists on data source, based on its username and password
+// value and returns a *User instance.
+func (ur *userRepository) FindByUsernameAndPassword(username, pwd string) (*User, error) {
+	var user User
+	result := ur.db.Where("username = ? AND password = ?", username, pwd).First(&user)
+	if result.Error != nil {
+		if result.Error == gorm.ErrRecordNotFound {
+			return nil, nil
+		}
+		return nil, result.Error
+	}
+
+	return &user, nil
 }
