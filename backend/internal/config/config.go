a61985677da5acd36f052c888717963de5aa1385
sacurio <sandy.acurio@gmail.com>
2023-08-22 20:19:25 -0500

    Extend `config` structure

--- backend/internal/config/config.go
+++ backend/internal/config/config.go
@@ -6,11 +6,23 @@ import (
 	"github.com/joho/godotenv"
 )
 
-// AppConfig handles all the chatbot app needed configuration.
-type AppConfig struct {
-	Name string
-	Port string
-}
+type (
+	// DatabaseConfig handles all the Database info values needed to establish a connection.
+	DatabaseConfig struct {
+		User   string
+		Pwd    string
+		Port   string
+		Host   string
+		DbName string
+	}
+
+	// AppConfig handles all the chatbot app needed configuration.
+	AppConfig struct {
+		Name     string
+		Port     string
+		DBConfig DatabaseConfig
+	}
+)
 
 // LoadConfig loads all config values from specified sources.
 func LoadConfig() (*AppConfig, error) {
@@ -21,5 +33,12 @@ func LoadConfig() (*AppConfig, error) {
 	return &AppConfig{
 		Name: os.Getenv("APP_NAME"),
 		Port: os.Getenv("SERVER_PORT"),
+		DBConfig: DatabaseConfig{
+			User:   os.Getenv("MYSQL_USER"),
+			Pwd:    os.Getenv("MYSQL_PASSWORD"),
+			Port:   os.Getenv("MYSQL_PORT"),
+			Host:   os.Getenv("MYSQL_HOST"),
+			DbName: os.Getenv("MYSQL_DATABASE"),
+		},
 	}, nil
 }
