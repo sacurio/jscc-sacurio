83388cbf4401e5c97563f07774de79723664207e
sacurio <sandy.acurio@gmail.com>
2023-08-22 20:19:03 -0500

    Implement `user` validation method

--- backend/internal/app/user/user_validator.go
+++ backend/internal/app/user/user_validator.go
@@ -1,5 +1,7 @@
 package user
 
+import "errors"
+
 type (
 	// UserValidator determines the methods to be implemented by userValidator struct.
 	UserValidator interface {
@@ -14,7 +16,23 @@ func NewUserValidator() UserValidator {
 	return &userValidator{}
 }
 
-// Validate probes if a user exists or not on data source, returning a custom error in negative case.
-func (u *userValidator) Validate(user *User) error {
+// Validate probes if a user info is correct before to be sent to repository.
+func (uv *userValidator) Validate(user *User) error {
+	if user == nil {
+		return errors.New("user is nil")
+	}
+
+	if user.Username == "" {
+		return errors.New("username is required")
+	}
+
+	if user.Email == "" {
+		return errors.New("email is required")
+	}
+
+	if user.Pwd == "" {
+		return errors.New("password is required")
+	}
+
 	return nil
 }
