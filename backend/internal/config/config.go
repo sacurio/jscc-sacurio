5e3db3c4978c8e7f583356048c574ce33778faa5
sacurio <sandy.acurio@gmail.com>
2023-08-23 00:22:14 -0500

    Add `RabbitMQ` configuration values

--- backend/internal/config/config.go
+++ backend/internal/config/config.go
@@ -19,15 +19,29 @@ type (
 
 	// AppConfig handles all the chatbot app needed configuration.
 	AppConfig struct {
-		Name     string
+		Name      string
+		Port      string
+		DBConfig  DatabaseConfig
+		RMQConfig RabbitMQConfig
+	}
+
+	// RabbitMQConfig handles allthe RabbitMQ configuration.
+	RabbitMQConfig struct {
+		User     string
+		Password string
+		Host     string
 		Port     string
-		DBConfig DatabaseConfig
 	}
 )
 
 // LoadConfig loads all config values from specified sources.
 func LoadConfig() (*AppConfig, error) {
-	if err := godotenv.Load(".env"); err != nil {
+	file := ".env-local"
+	if os.Getenv("ENVIRONMENT") != "" {
+		file = ".env"
+	}
+
+	if err := godotenv.Load(file); err != nil {
 		return nil, err
 	}
 
@@ -40,6 +54,12 @@ func LoadConfig() (*AppConfig, error) {
 			Port:   os.Getenv("MYSQL_PORT"),
 			Host:   util.ValidateStringNotEmpty(os.Getenv("MYSQL_HOST"), "localhost"),
 			DbName: os.Getenv("MYSQL_DATABASE"),
+		},
+		RMQConfig: RabbitMQConfig{
+			User:     os.Getenv("RABBITMQ_DEFAULT_USER"),
+			Password: os.Getenv("RABBITMQ_DEFAULT_PASS"),
+			Host:     util.ValidateStringNotEmpty(os.Getenv("RABBITMQ_HOST"), "localhost"),
+			Port:     os.Getenv("RABBITMQ_DEFAULT_PORT"),
 		},
 	}, nil
 }
