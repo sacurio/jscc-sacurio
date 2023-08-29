d063dd44cea1bbf0bf4243c87d6126f83252aff3
sacurio <sandy.acurio@gmail.com>
2023-08-29 11:16:11 -0500

    Avoid to expose sensitive information

--- backend/internal/app/model/message.go
+++ backend/internal/app/model/message.go
@@ -4,7 +4,7 @@ type Message struct {
 	ID        uint   `gorm:"type:int;primaryKey;autoIncrement;not null" json:"id"`
 	Content   string `gorm:"type:nvarchar(1000);not null" json:"content"`
 	Timestamp int64  `gorm:"type:int;not null" json:"timestamp"`
-	UserID    uint   `gorm:"type:int;not null"`
+	UserID    uint   `gorm:"type:int;not null" json:"-"`
 	User      User   `gorm:"foreignKey:UserID"`
 }
 
--- backend/internal/app/model/user.go
+++ backend/internal/app/model/user.go
@@ -2,8 +2,8 @@ package model
 
 // User represents the user info structure.
 type User struct {
-	ID       uint   `gorm:"type:int;primaryKey"`
-	Username string `gorm:"type:varchar(50);not null;size:50;index"`
-	Email    string `gorm:"type:varchar(100);not null;size:100"`
-	Pwd      string `gorm:"type:varchar(32);not null;size:32"`
+	ID       uint   `gorm:"type:int;primaryKey" json:"-"`
+	Username string `gorm:"type:varchar(50);not null;size:50;index" json:"username"`
+	Email    string `gorm:"type:varchar(100);not null;size:100" json:"email"`
+	Pwd      string `gorm:"type:varchar(32);not null;size:32" json:"-"`
 }
