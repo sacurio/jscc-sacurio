b08dabce615b02c4424a165e35b176c9ff7d33d2
sacurio <sandy.acurio@gmail.com>
2023-08-29 00:36:22 -0500

    Update file name

--- backend/pkg/db/init/01.sql
+++ backend/pkg/db/init/01.sql
@@ -12,5 +12,7 @@ CREATE TABLE IF NOT EXISTS users (
     Seed user data 
 */
 INSERT INTO users(username, email, pwd) VALUES
+    ('bot', 'bot@chat.com', 'fabcaa97871555b68aa095335975e613'),
     ('alice', 'alice@mail.com', '6384e2b2184bcbf58eccf10ca7a6563c'),
-    ('bob', 'bob@mail.com', '9f9d51bc70ef21ca5c14f307980a29d8');
+    ('bob', 'bob@mail.com', '9f9d51bc70ef21ca5c14f307980a29d8'),
+    ('louis', 'louis@mail.com', '777cadc280bb23ebea268ded98338c39');
