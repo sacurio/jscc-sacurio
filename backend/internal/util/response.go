a71d41a8ab740927fa25781b28aa3e3e709fa8b5
sacurio <sandy.acurio@gmail.com>
2023-08-29 00:41:15 -0500

    Add `auth` headers

--- backend/internal/util/response.go
+++ backend/internal/util/response.go
@@ -18,18 +18,26 @@ func SendErrorResponse(w http.ResponseWriter, statusCode int, err string) {
 
 	errJSON, errMsg := json.Marshal(dataErr)
 	if errMsg != nil {
-		sendResponse(w, http.StatusInternalServerError, []byte(errMsg.Error()))
+		sendResponse(w, http.StatusInternalServerError, []byte(errMsg.Error()), nil)
 	}
 
-	sendResponse(w, statusCode, errJSON)
+	sendResponse(w, statusCode, errJSON, nil)
 }
 
 // SendJSONResponse writes data to ResponseWriter and setting the http status code received as well.
 func SendJSONResponse(w http.ResponseWriter, statusCode int, data []byte) {
-	sendResponse(w, statusCode, data)
+	sendResponse(w, statusCode, data, nil)
 }
 
-func sendResponse(w http.ResponseWriter, statusCode int, data []byte) {
+func SendJSONResponseWithHeaders(w http.ResponseWriter, statusCode int, data []byte, headers map[string]string) {
+	sendResponse(w, statusCode, data, headers)
+}
+
+func sendResponse(w http.ResponseWriter, statusCode int, data []byte, headers map[string]string) {
+	for h, v := range headers {
+		w.Header().Set(h, v)
+	}
+
 	w.WriteHeader(statusCode)
 	w.Write(data)
 }
